df0024346a723f5077bee4ae06872c06f44c3a17
Muhammad Usman <m.usman@platalytics.com>
2019-04-16 22:05:35 +0500

    frontend base jar issue resolved edit pom class added

--- codegen/src/main/java/com/nfinity/codegen/FronendBaseTemplateGenerator.java
+++ codegen/src/main/java/com/nfinity/codegen/FronendBaseTemplateGenerator.java
@@ -7,18 +7,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.CodeSource;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.IOCase;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import freemarker.cache.ClassTemplateLoader;
@@ -33,55 +51,29 @@ public class FronendBaseTemplateGenerator {
 
 	public static void generate(String destination, String clientSubfolder) {
 		String command = "ng new client --skipInstall=true";
-		runCommand(command, destination);
-		System.out.println(System.getProperty("user.dir"));
+		//		runCommand(command, destination);
 
-		// create client folder if it doesn't exist
-
-		generate(
-				System.getProperty("user.dir").replace("\\", "/")
-						+ "/src/main/resources/templates/frontendBaseTemplate",
-				FRONTEND_BASE_TEMPLATE_FOLDER, destination + "/"+ clientSubfolder + "/");
-		// generate("F:/projects/New
-		// folder/codegen/codegen/src/main/resources/templates/frontendBaseTemplate",FRONTEND_BASE_TEMPLATE_FOLDER,
-		// destination + "/generatedProject/src/app");
-
-	}
-
-	private static void generate(String path, String parent, String destination) {
-		File[] fl = getFilesFromFolder(path);
+		List<String> fl = getFilesFromFolder(FRONTEND_BASE_TEMPLATE_FOLDER);
 		Map<String, Object> templates = new HashMap<>();
 
-		ClassTemplateLoader ctl = new ClassTemplateLoader(CodegenApplication.class, parent + "/");
+		ClassTemplateLoader ctl = new ClassTemplateLoader(CodegenApplication.class, FRONTEND_BASE_TEMPLATE_FOLDER + "/");
 		TemplateLoader[] templateLoadersArray = new TemplateLoader[] { ctl };
 		MultiTemplateLoader mtl = new MultiTemplateLoader(templateLoadersArray);
 		cfg.setDefaultEncoding("UTF-8");
 		cfg.setInterpolationSyntax(Configuration.SQUARE_BRACKET_INTERPOLATION_SYNTAX);
 		cfg.setTemplateLoader(mtl);
 
-		if (fl.length > 0) {
 
-			for (File file : fl) {
-				String filename = file.getName();
-				templates.put(filename, filename.substring(0, filename.lastIndexOf('.')));
-			}
+
+		for (String filePath : fl) {
+			String p = filePath.replace("BOOT-INF/classes" + FRONTEND_BASE_TEMPLATE_FOLDER,"");
+			p = p.replace("\\", "/");
+			p = p.replace(System.getProperty("user.dir").replace("\\", "/") + "/src/main/resources" + FRONTEND_BASE_TEMPLATE_FOLDER,"");
+			templates.put(p, p.substring(0, p.lastIndexOf('.')));
 		}
 
-		generateFiles(templates, null, destination);
 
-		File[] folderList = getNestedFolders(path);
-		for (File folder : folderList) {
-			String folderName = folder.getName();
-			String newPath = path + "/" + folderName;
-			String newParent = parent + "/" + folderName;
-			String newDestination = destination + "/" + folderName;
-
-			File directory = new File(newDestination);
-			if (!directory.exists()) {
-				directory.mkdir();
-			}
-			generate(newPath, newParent, newDestination);
-		}
+		generateFiles(templates, null, destination + "/"+ clientSubfolder);
 
 	}
 
@@ -90,8 +82,17 @@ public class FronendBaseTemplateGenerator {
 			try {
 				Template template = cfg.getTemplate(entry.getKey());
 
-				File fileName = new File(destPath + "/" + entry.getValue().toString()); /// new File(destPath + "/" +
-																						/// entry.getValue().toString());
+				String entryPath = entry.getValue().toString();
+				File fileName = new File(destPath + "/" + entryPath); /// new File(destPath + "/" +
+				String dirPath = destPath;
+				if(destPath.split("/").length > 1) {
+					dirPath = dirPath + entryPath.substring(0, entryPath.lastIndexOf('/'));
+				}
+				System.out.println(dirPath);
+				File dir = new File(dirPath);
+				if(!dir.exists()) {
+					dir.mkdirs();
+				};
 				PrintWriter writer = new PrintWriter(fileName);
 				template.process(root, writer);
 				writer.flush();
@@ -132,13 +133,60 @@ public class FronendBaseTemplateGenerator {
 		}
 	}
 
-	public static File[] getFilesFromFolder(String folderPath) {
+	public static List<String> getFilesFromFolder(String folderPath) {
 
-		File dir = new File(folderPath);
-		FileFilter fileFilter = new WildcardFileFilter("*.ftl", IOCase.INSENSITIVE);
-		File[] fileList = dir.listFiles(fileFilter);
+		try {
+			Path myPath;
+			URI uri = FronendBaseTemplateGenerator.class.getResource(folderPath).toURI();
+			List<String> list = new ArrayList<String>();
+			if (uri.getScheme().equals("jar")) {
+				list = getFilesFromJar(folderPath);
+			} else {
+				Collection<File> files = getFilesFromFileSystem(new File(System.getProperty("user.dir").replace("\\", "/") + "/src/main/resources/"+ folderPath));
+				for(File file:files) {
+					if(file.isFile())
+						list.add(file.getAbsolutePath());
+				}
+			}
+			return list;
 
-		return fileList;
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static Collection<File> getFilesFromFileSystem(File path){
+		Collection<File> files = org.apache.commons.io.FileUtils.listFilesAndDirs(
+				path,
+				new RegexFileFilter("^(.*?)"), 
+				DirectoryFileFilter.DIRECTORY
+				);
+		return files;
+	}
+
+	private static List<String> getFilesFromJar(String path) throws IOException{
+		CodeSource src = FronendBaseTemplateGenerator.class.getProtectionDomain().getCodeSource();
+		List<String> list = new ArrayList<String>();
+		if( src != null ) {
+			java.net.URL jar = src.getLocation();
+			ZipInputStream zip = new ZipInputStream( jar.openStream());
+			ZipEntry ze = null;
+
+			while( ( ze = zip.getNextEntry() ) != null ) {
+				String entryName = ze.getName();
+				if( entryName.startsWith("BOOT-INF/classes" + path) && entryName.endsWith(".ftl") ) {
+					list.add( entryName );
+				}
+			}
+		}
+		return list;
 	}
 
 	public static File[] getNestedFolders(String folderPath) {
